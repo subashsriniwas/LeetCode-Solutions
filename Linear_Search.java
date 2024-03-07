ab123b293efeefb6edaabf19d586e6b479bb6f75
Subash <132041545+subashsriniwas@users.noreply.github.com>
2024-03-07 12:05:23 +0530

    Updated Succesfully.

--- Linear_Search.java
+++ Linear_Search.java
@@ -4,8 +4,6 @@ public class Linear_Search {
         System.out.println(linearSearch(nums, 4));
     }
     
-
-    
     private static int linearSearch (int[] nums, int target) {
         for (int i=0; i<nums.length; i++) {
             if (nums[i] == target) { return i; }
